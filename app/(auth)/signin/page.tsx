b6758da47bb7abeb069325e2b8bdd95ddda020d2
aniiaham <aniya_axm@icloud.com>
2023-06-01 13:04:44 -0400

    setup: styles

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,16 +1,3 @@
-import "@/styles/global.css";
-
-import GlassPane from "@/components/GlassPane";
-
-export default function AuthRootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head />
-      <body className="h-screen q-screen rainbow-mesh p-6">
-        <GlassPane className="w-full h-full flex items-center justify-center">
-          {children}
-        </GlassPane>
-      </body>
-    </html>
-  );
+export default function SignIn() {
+  return <div>signin</div>
 }
