0a138f395382eec8ac7e14fc4d55563a7e261f7e
aniiaham <aniya_axm@icloud.com>
2023-06-02 16:01:31 -0400

    created: sidebar comp

---
+++ components/SideBar.tsx
@@ -0,0 +1,20 @@
+import Card from "./Card";
+// import Image from "next/image";
+import SidebarLink from "./SideBarLink";
+
+const links = [
+  { label: "Home", icon: "Grid", link: "/home" },
+  { label: "Calendar", icon: "Calendar", link: "/calendar" },
+  { label: "Profile", icon: "User", link: "/profile" },
+  { label: "Settings", icon: "Settings", link: "/settings" },
+];
+
+const Sidebar = () => {
+    <Card className="h-full w-40 flex items-center justify-between flex-wrap">
+        {links.map((link) => (
+            <SidebarLink link={link}/>
+        ))}
+        </Card>
+}
+
+export default Sidebar
